074eae8d0ac71eb48e3c996b93b4cad00b6cdea2
drskur <drskur@me.com>
2017-06-29 14:25:09 +0900

    FirstCommit

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+
+namespace Zang.Droid
+{
+    [Activity(Label = "Zang", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/MyTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+
+        }
+    }
+}
+
---
+++ Zang/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Zang
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
